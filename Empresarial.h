e2a1b0e48136917304c68dd222aa98f296156c84
HectorNLeon <32803433+HectorNLeon@users.noreply.github.com>
2017-11-16 17:21:36 -0600

    Add files via upload

---
+++ Empresarial.h
@@ -0,0 +1,60 @@
+#ifndef EMPRESARIAL_H_INCLUDED
+#define EMPRESARIAL_H_INCLUDEDE
+class Empresarial:public Servicio{
+    private:
+        double costoxDia;
+        int maximaCantidad;
+        string descripcion;
+    public:
+        Empresarial();
+        Empresarial(string, char, double, int, string);
+        void setCostoxDia(double);
+        void setMaximaCantidad(int);
+        void setDescripcion(string);
+        double getCostoxDia();
+        int getMaximaCantidad();
+        string getDescripcion();
+        void muestra();
+        double calcularCosto(duraHrs);
+};
+Empresarial::Empresarial():Servicio(){
+    costoxDia=0;
+    maximaCantidad=0;
+    descripcion=" ";
+}
+Empresarial::Empresarial(string c, char t, double cos, int m, string d):Servicio(c,t){
+    costoxDia=cos;
+    maximaCantidad=m;
+    descripcion=d;
+}
+void Empresarial::setCostoxDia(double cos){
+    costoxDia=cos;
+}
+void Empresarial::setMaximaCantidad(int m){
+    maximaCantidad=m;
+}
+void Empresarial::setDescripcion(string d){
+    descripcion=d;
+}
+double Empresarial::getCostoxDia(){
+    return costoxDia;
+}
+int Empresarial::getMaximaCantidad(){
+    return maximaCantidad;
+}
+string Empresarial::getDescripcion(){
+    return descripcion;
+}
+void Empresarial::vmuestra(){
+    cout<<"Clave "<<cveServicio<<"\nTipo "<<tipo<<"\nCosto por dia "<<costoxDia<<"\nMaxima cantidad "<<maximaCantidad<<"\nDescripcion "<<descripcion<<endl;
+}
+double Empresarial::calcularCosto(duraHrs){
+    return costoxDia*duraHrs/8;
+
+}
+
+
+
+
+
+#endif // EMPRESARIAL_H_INCLUDED
